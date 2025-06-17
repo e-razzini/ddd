522967ab756651ffeaec0b3a70e75cc35a8e7491
elton razzini <eltonrazzini@hotmail.com>
2025-06-17 19:17:02 -0300

    repository de Ordem

---
+++ src/infrastructure/repository/order.repository.ts
@@ -0,0 +1,41 @@
+import orderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import Order from "../../domain/entity/order";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
+
+export default class OrderRepository {
+//implements orderRepositoryInterface  {
+
+
+        async create(entity:Order):Promise<void> {
+
+            await OrderModel.create({
+              id         : entity.id,
+              customer_id: entity.customer_id,
+              total      : entity.total,
+              items      : entity.items.map((item)=>({
+                id         :item.id,
+                name       :item.name,
+                price      :item.price,
+                product_id :item.product_id,
+                quantity   :item.quantity
+             })),
+             },
+             {
+              include:[{model:OrderItemModel}]
+             }
+            );
+        }
+        /*
+
+        async updated(entity:Order):Promise<void>{
+        }
+
+        async find(id:string):Promise<Order>{
+
+        }
+
+        async find_all():Promise<Order[]>{
+        }*/
+
+}
