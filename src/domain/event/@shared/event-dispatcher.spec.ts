f6da087c221aff34dc20ce5057b400999be21b63
elton razzini <eltonrazzini@hotmail.com>
2025-07-08 21:48:07 -0300

    criacao de testes novos

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,13 +1,19 @@
-import EventDispatcher from "./event-dispatcher";
+import EventDispatcher          from "./event-dispatcher";
 import SendEmailCustomerHandler from "../customer/handler/send-mail-customer.handler";
-import CustomerCreateEvent from "../customer/customer-create.event";
+import CreateCustomerHandler    from "../customer/handler/create-message-customer.handler";
+import AlteredCustomerHandler   from "../customer/handler/alter-message-customer.handler";
 
-import SendEmailProductHandler from "../product/handler/send-email-product.handler";
-import ProductCreateEvent from "../product/product-create.event";   
+import CustomerCreateEvent      from "../customer/customer-create.event";
+import CustomerAlteredEvent     from "../customer/customer-altered.event";
+
+import SendEmailProductHandler        from "../product/handler/send-email-product.handler";
+import ProductCreateEvent             from "../product/product-create.event";   
+import SendEmailCustomerFriendHandler from "../customer/handler/send-message-friend-customer.handler";
+
+import Customer from "../../entity/customer";
+import Address  from "../../entity/address"; 
 
 describe("Dominio de Eventos - EventDispatcher", () => {
-
-
 
        test.skip("Register evento handler",() =>{
 
@@ -20,7 +26,6 @@ describe("Dominio de Eventos - EventDispatcher", () => {
            expect(eventDispatcher.getEventHandlers["SendEmailCustomerHandler"].length).toBe(1);
            expect(eventDispatcher.getEventHandlers["SendEmailCustomerHandler"][0]).toMatchObject(eventHandler);
        });
-
        test.skip("Unregister evento handler",() =>{
            
            const eventDispatcher = new EventDispatcher();
@@ -36,7 +41,6 @@ describe("Dominio de Eventos - EventDispatcher", () => {
            expect(eventDispatcher.getEventHandlers["SendEmailCustomerHandler"].length).toBe(0);
            
        });
-
        test.skip("Unregister all event handlers",() =>{
            
            const eventDispatcher = new EventDispatcher();
@@ -52,8 +56,6 @@ describe("Dominio de Eventos - EventDispatcher", () => {
            expect(eventDispatcher.getEventHandlers["SendEmailCustomerHandler"]).toBeUndefined();
            
        });  
-
-
        test.skip("Notify event handler",() =>{
            
            const eventDispatcher = new EventDispatcher();
@@ -77,8 +79,7 @@ describe("Dominio de Eventos - EventDispatcher", () => {
            expect(spy).toHaveBeenCalled();
            
        });  
-
-       test("Register event product handler",() =>{
+       test.skip("Register event product handler",() =>{
 
            const eventDispatcher = new EventDispatcher();
            const eventHandler    = new SendEmailProductHandler();
@@ -91,8 +92,7 @@ describe("Dominio de Eventos - EventDispatcher", () => {
 
 
        });
-
-       test("Unregister event product handler",() =>{
+       test.skip("Unregister event product handler",() =>{
 
 
           const eventDispatcher = new EventDispatcher();
@@ -109,8 +109,7 @@ describe("Dominio de Eventos - EventDispatcher", () => {
 
 
        });
-
-       test("Unregister all event product handlers",() =>{
+       test.skip("Unregister all event product handlers",() =>{
 
           const eventDispatcher = new EventDispatcher();
           const eventHandler    = new SendEmailProductHandler();        
@@ -125,8 +124,7 @@ describe("Dominio de Eventos - EventDispatcher", () => {
             expect(eventDispatcher.getEventHandlers["SendEmailProductHandler"]).toBeUndefined();
 
        });
-    
-       test("Notify event product handler",() =>{
+       test.skip("Notify event product handler",() =>{
 
           const eventDispatcher = new EventDispatcher();
           const eventHandler    = new SendEmailProductHandler();
@@ -148,5 +146,122 @@ describe("Dominio de Eventos - EventDispatcher", () => {
             expect(spy).toHaveBeenCalled();
 
 
-        });
+       });
+       test.skip("Register send message friend customer handler", () => {
+           const eventDispatcher = new EventDispatcher();
+           const eventHandler    = new SendEmailCustomerFriendHandler();
+           
+           // Registering the event handler this class name
+           eventDispatcher.register("SendEmailCustomerFriendHandler", eventHandler);
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"]).toBeDefined();
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"].length).toBe(1);
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"][0]).toMatchObject(eventHandler);
+       });  
+       test.skip("Unregister send message friend customer handler", () => {
+           const eventDispatcher = new EventDispatcher();
+           const eventHandler    = new SendEmailCustomerFriendHandler();
+           
+           // Registering the event handler this class name
+           eventDispatcher.register("SendEmailCustomerFriendHandler", eventHandler);
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"]).toBeDefined();
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"].length).toBe(1);
+           
+           // Unregistering the event handler this class name
+           eventDispatcher.unregister("SendEmailCustomerFriendHandler", eventHandler);
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"].length).toBe(0);
+       });  
+       test.skip("Unregister all send message friend customer handlers", () => {
+           const eventDispatcher = new EventDispatcher();
+           const eventHandler    = new SendEmailCustomerFriendHandler();
+           
+           // Registering the event handler this class name
+           eventDispatcher.register("SendEmailCustomerFriendHandler", eventHandler);
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"]).toBeDefined();
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"].length).toBe(1);
+           
+           // Unregistering all event handlers
+           eventDispatcher.unregisterAll();
+           expect(eventDispatcher.getEventHandlers["SendEmailCustomerFriendHandler"]).toBeUndefined();
+       });      
+       test.skip("Notify send message friend customer handler", () => {
+           const eventDispatcher       = new EventDispatcher();
+           const eventHandler          = new SendEmailCustomerFriendHandler();
+           const spy_send_email_friend = jest.spyOn(eventHandler, "handle");
+           
+           // Registering the event handler this class name
+           eventDispatcher.register("CustomerCreateEvent", eventHandler);
+           expect(eventDispatcher.getEventHandlers["CustomerCreateEvent"][0]).toMatchObject(eventHandler);
+
+           // customer
+            const customerCreateEvent = new CustomerCreateEvent({
+            name :"Customer 1",
+            describe: "Customer 1 description",
+            email_friend:"myfrient@gmail.com.br"});
+            
+            // Notify the event handler       
+             eventDispatcher.notify(customerCreateEvent);  
+               expect(spy_send_email_friend).toHaveBeenCalled();           
+       });
+
+       test.skip("Notify  create customer handler with no event handlers", () => {
+           const eventDispatcher = new EventDispatcher();
+           const eventHandler    = new CreateCustomerHandler();
+           const spy = jest.spyOn(eventHandler, "handle");
+           
+           // Registering the event handler this class name
+           eventDispatcher.register("CustomerCreateEvent", eventHandler);
+           expect(eventDispatcher.getEventHandlers["CustomerCreateEvent"][0]).toMatchObject(eventHandler);
+
+            // customer
+            const customer = new Customer('1', 'Customer 1 ');
+            const address  = new Address('Rua 1', '123', 'Bairro 1','Estado 1');
+            customer.setAddress(address);
+
+            const customerCreateEvent = new CustomerCreateEvent(customer);
+            eventDispatcher.notify(customerCreateEvent);
+            expect(spy).toHaveBeenCalled();
+     
+            const eventHandler_2    = new AlteredCustomerHandler();
+            // Registering the event handler this class name
+           eventDispatcher.register("CustomerCreateEvent", eventHandler_2);
+           expect(eventDispatcher.getEventHandlers["CustomerCreateEvent"][0]).toMatchObject(eventHandler_2);
+
+            const address_2  = new Address('Rua 2','32443', 'Bairro 2','Estado2');
+            customer.setAddress(address_2);
+
+            const customerAlteredEvent = new CustomerAlteredEvent(customer);
+            eventDispatcher.notify(customerAlteredEvent);
+            expect(spy).toHaveBeenCalled();
+
+        });    
+       
+        test("Notify  altered customer handler with no event handlers", () => {
+           const eventDispatcher = new EventDispatcher();
+           const eventHandler    = new CreateCustomerHandler();
+
+           const alteredEventHandler  = new AlteredCustomerHandler();
+           const spy = jest.spyOn(alteredEventHandler, "handle");
+           
+           // Registering the event handler this class name
+           eventDispatcher.register("CustomerCreateEvent", eventHandler);
+           expect(eventDispatcher.getEventHandlers["CustomerCreateEvent"][0]).toMatchObject(eventHandler);
+           // customer
+           const customer = new Customer('1', 'Customer 1 ');
+           const address  = new Address('Rua 1', '123', 'Bairro 1','Estado 1');
+           customer.setAddress(address);
+           const customerCreateEvent = new CustomerCreateEvent(customer);
+           eventDispatcher.notify(customerCreateEvent);
+                  
+            // Registering the event handler this class name
+           eventDispatcher.register("CustomerAlteredEvent", alteredEventHandler);
+           expect(eventDispatcher.getEventHandlers["CustomerAlteredEvent"][0]).toMatchObject(alteredEventHandler);
+
+            const address_2  = new Address('Rua 2','32443', 'Bairro 2','Estado2');
+            customer.setAddress(address_2);
+
+            const customerAlteredEvent = new CustomerAlteredEvent(customer);
+            eventDispatcher.notify(customerAlteredEvent);
+            expect(spy).toHaveBeenCalled();
+        });   
+
 });
